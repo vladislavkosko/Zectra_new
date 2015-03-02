0e5741a6a4f8709537c8499debba910b4061d6eb
PilliVilli <andriismuzhanitsya@gmail.com>
2015-03-02 17:19:29 +0200

    chatcontroller

---
+++ web/bundles/zectranet/js/custom/chatController.js
@@ -0,0 +1,5 @@
+var chat = Zectranet.controller('ChatController', ['$rootScope', '$scope', '$http', '$paginator', '$modal', '$sce',
+    function($rootScope, $scope, $http, $paginator, $modal, $sce) {
+
+        console.log('Chat Controller was loaded');
+    }]);
