1b579f49fe89c2316f3a8f23edd6c6b60adee1da
riki343 <riki34@spaces.ru>
2015-04-17 16:23:59 +0300

    Send contact request

--- src/ZectranetBundle/Entity/Conversation.php
+++ src/ZectranetBundle/Entity/Conversation.php
@@ -2,6 +2,7 @@
 
 namespace ZectranetBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -22,12 +23,54 @@ class Conversation
      */
     private $id;
 
+    /**
+     * @var int
+     * @ORM\Column(name="user1_id", type="integer")
+     */
     private $user1ID;
+
+    /**
+     * @var User
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user1_id", referencedColumnName="id")
+     */
     private $user1;
+
+    /**
+     * @var int
+     * @ORM\Column(name="user2_id", type="integer")
+     */
     private $user2ID;
+
+    /**
+     * @var User
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user2_id", referencedColumnName="id")
+     */
     private $user2;
+
+    /**
+     * @var int
+     * @ORM\Column(name="office_id", type="integer")
+     */
     private $officeID;
+
+    /**
+     * @var Office
+     * @ORM\ManyToOne(targetEntity="Office")
+     * @ORM\JoinColumn(name="office_id", referencedColumnName="id")
+     */
+    private $office;
+
+    /**
+     * @var ArrayCollection
+     * @ORM\OneToMany(targetEntity="ConversationMessage", mappedBy="conversation")
+     */
     private $messages;
+
+    public function __construct() {
+        $this->messages = new ArrayCollection();
+    }
 
     /**
      * Get id
@@ -37,5 +80,176 @@ class Conversation
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set user1ID
+     *
+     * @param integer $user1ID
+     * @return Conversation
+     */
+    public function setUser1ID($user1ID)
+    {
+        $this->user1ID = $user1ID;
+
+        return $this;
+    }
+
+    /**
+     * Get user1ID
+     *
+     * @return integer 
+     */
+    public function getUser1ID()
+    {
+        return $this->user1ID;
+    }
+
+    /**
+     * Set user2ID
+     *
+     * @param integer $user2ID
+     * @return Conversation
+     */
+    public function setUser2ID($user2ID)
+    {
+        $this->user2ID = $user2ID;
+
+        return $this;
+    }
+
+    /**
+     * Get user2ID
+     *
+     * @return integer 
+     */
+    public function getUser2ID()
+    {
+        return $this->user2ID;
+    }
+
+    /**
+     * Set officeID
+     *
+     * @param integer $officeID
+     * @return Conversation
+     */
+    public function setOfficeID($officeID)
+    {
+        $this->officeID = $officeID;
+
+        return $this;
+    }
+
+    /**
+     * Get officeID
+     *
+     * @return integer 
+     */
+    public function getOfficeID()
+    {
+        return $this->officeID;
+    }
+
+    /**
+     * Set user1
+     *
+     * @param \ZectranetBundle\Entity\User $user1
+     * @return Conversation
+     */
+    public function setUser1(\ZectranetBundle\Entity\User $user1 = null)
+    {
+        $this->user1 = $user1;
+
+        return $this;
+    }
+
+    /**
+     * Get user1
+     *
+     * @return \ZectranetBundle\Entity\User 
+     */
+    public function getUser1()
+    {
+        return $this->user1;
+    }
+
+    /**
+     * Set user2
+     *
+     * @param \ZectranetBundle\Entity\User $user2
+     * @return Conversation
+     */
+    public function setUser2(\ZectranetBundle\Entity\User $user2 = null)
+    {
+        $this->user2 = $user2;
+
+        return $this;
+    }
+
+    /**
+     * Get user2
+     *
+     * @return \ZectranetBundle\Entity\User 
+     */
+    public function getUser2()
+    {
+        return $this->user2;
+    }
+
+    /**
+     * Set office
+     *
+     * @param \ZectranetBundle\Entity\Office $office
+     * @return Conversation
+     */
+    public function setOffice(\ZectranetBundle\Entity\Office $office = null)
+    {
+        $this->office = $office;
+
+        return $this;
+    }
+
+    /**
+     * Get office
+     *
+     * @return \ZectranetBundle\Entity\Office 
+     */
+    public function getOffice()
+    {
+        return $this->office;
+    }
+
+    /**
+     * Add messages
+     *
+     * @param \ZectranetBundle\Entity\ConversationMessage $messages
+     * @return Conversation
+     */
+    public function addMessage(\ZectranetBundle\Entity\ConversationMessage $messages)
+    {
+        $this->messages[] = $messages;
+
+        return $this;
+    }
+
+    /**
+     * Remove messages
+     *
+     * @param \ZectranetBundle\Entity\ConversationMessage $messages
+     */
+    public function removeMessage(\ZectranetBundle\Entity\ConversationMessage $messages)
+    {
+        $this->messages->removeElement($messages);
+    }
+
+    /**
+     * Get messages
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getMessages()
+    {
+        return $this->messages;
     }
 }
--- src/ZectranetBundle/Entity/ConversationMessage.php
+++ src/ZectranetBundle/Entity/ConversationMessage.php
@@ -21,13 +21,54 @@ class ConversationMessage
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
+
+    /**
+     * @var int
+     * @ORM\Column(name="user_id", type="integer")
+     */
     private $userID;
+
+    /**
+     * @var User
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
     private $user;
+
+    /**
+     * @var int
+     * @ORM\Column(name="conversation_id", type="integer")
+     */
     private $conversationID;
+
+    /**
+     * @var Conversation
+     * @ORM\ManyToOne(targetEntity="Conversation", inversedBy="messages")
+     * @ORM\JoinColumn(name="conversation_id", referencedColumnName="id")
+     */
     private $conversation;
+
+    /**
+     * @var string
+     * @ORM\Column(name="message", type="string", length=3000)
+     */
     private $message;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="posted", type="datetime")
+     */
     private $posted;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="edited", type="datetime", nullable=true, options={"default" = null})
+     */
     private $edited;
+
+    public function __construct() {
+        $this->posted = new \DateTime();
+    }
 
     /**
      * Get id
@@ -37,5 +78,166 @@ class ConversationMessage
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set userID
+     *
+     * @param integer $userID
+     * @return ConversationMessage
+     */
+    public function setUserID($userID)
+    {
+        $this->userID = $userID;
+
+        return $this;
+    }
+
+    /**
+     * Get userID
+     *
+     * @return integer 
+     */
+    public function getUserID()
+    {
+        return $this->userID;
+    }
+
+    /**
+     * Set conversationID
+     *
+     * @param string $conversationID
+     * @return ConversationMessage
+     */
+    public function setConversationID($conversationID)
+    {
+        $this->conversationID = $conversationID;
+
+        return $this;
+    }
+
+    /**
+     * Get conversationID
+     *
+     * @return string 
+     */
+    public function getConversationID()
+    {
+        return $this->conversationID;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return ConversationMessage
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string 
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set posted
+     *
+     * @param \DateTime $posted
+     * @return ConversationMessage
+     */
+    public function setPosted($posted)
+    {
+        $this->posted = $posted;
+
+        return $this;
+    }
+
+    /**
+     * Get posted
+     *
+     * @return \DateTime 
+     */
+    public function getPosted()
+    {
+        return $this->posted;
+    }
+
+    /**
+     * Set edited
+     *
+     * @param \DateTime $edited
+     * @return ConversationMessage
+     */
+    public function setEdited($edited)
+    {
+        $this->edited = $edited;
+
+        return $this;
+    }
+
+    /**
+     * Get edited
+     *
+     * @return \DateTime 
+     */
+    public function getEdited()
+    {
+        return $this->edited;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \ZectranetBundle\Entity\User $user
+     * @return ConversationMessage
+     */
+    public function setUser(\ZectranetBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \ZectranetBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set conversation
+     *
+     * @param \ZectranetBundle\Entity\Conversation $conversation
+     * @return ConversationMessage
+     */
+    public function setConversation(\ZectranetBundle\Entity\Conversation $conversation = null)
+    {
+        $this->conversation = $conversation;
+
+        return $this;
+    }
+
+    /**
+     * Get conversation
+     *
+     * @return \ZectranetBundle\Entity\Conversation 
+     */
+    public function getConversation()
+    {
+        return $this->conversation;
     }
 }
