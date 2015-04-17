8c44fa79dac36cfb2ed84d69665eaf3a1733fcca
PilliVilli <andriismuzhanitsya@gmail.com>
2015-04-17 16:36:05 +0200

    SendConversationMessage

--- web/bundles/zectranet/js/custom/homeOfficeController.js
+++ web/bundles/zectranet/js/custom/homeOfficeController.js
@@ -1,15 +1,46 @@
 Zectranet.controller('HomeOfficeController', ['$scope', '$http',
     function($scope, $http) {
-        $scope.urlGetContactList = function () {
-            $scope.urlGetContactList = JSON_URLS.getContactList;
 
-            $scope.getContactList = function () {
-                $http.get($scope.urlGetContactList)
-                    .success(function (response) {
-                        /// Bla bla bla
+        $scope.urlGetContactList = JSON_URLS.getContactList;
+        $scope.urlGetConversation = JSON_URLS.getConversation;
+        $scope.urlSendConversationMessage = JSON_URLS.urlSendConversationMessage;
+        $scope.contacts = [];
+        $scope.conversation = null;
+        $scope.asset = JSON_URLS.asset;
+        $scope.message = '';
+
+        $scope.getContactList = function () {
+            $http.get($scope.urlGetContactList)
+                .success(function (response) {
+                    $scope.contacts = response;
+                    for(var i=0; i<$scope.contacts.length;i++ )
+                    {
+                        $scope.contacts[i].checked = false;
                     }
-                );
-            };
+                }
+            );
         };
+        
+        $scope.getConversation = function (id) {
+
+            for(var i=0; i<$scope.contacts.length;i++ )
+            {
+                $scope.contacts[i].checked = ($scope.contacts[i].id == id) ;
+            }
+            $http.get($scope.urlGetConversation.replace('0' , id))
+                .success(function (response) {
+                    $scope.conversation = response;
+                });
+        };
+
+        $scope.SendConversationMessage = function (message,conversation_id) {
+            $scope.message = '';
+            $http.post($scope.urlSendConversationMessage.replace('0',conversation_id), {'message': message})
+                .success(function (response) {
+
+                })
+
+
+        }
     }
 ]);
