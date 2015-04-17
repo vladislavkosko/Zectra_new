91d6d9113eba3741747de4595eea635d125136c9
riki343 <riki34@spaces.ru>
2015-04-17 14:45:20 +0300

    Send contact request

--- web/bundles/zectranet/js/custom/homeOfficeController.js
+++ web/bundles/zectranet/js/custom/homeOfficeController.js
@@ -1,7 +1,15 @@
 Zectranet.controller('HomeOfficeController', ['$scope', '$http',
     function($scope, $http) {
         $scope.urlGetContactList = function () {
+            $scope.urlGetContactList = JSON_URLS.getContactList;
 
+            $scope.getContactList = function () {
+                $http.get($scope.urlGetContactList)
+                    .success(function (response) {
+                        /// Bla bla bla
+                    }
+                );
+            };
         };
     }
 ]);
