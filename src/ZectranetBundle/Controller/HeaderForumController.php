360c0f724bd4a41c87420ab999b6ade8cccf2a29
riki343 <riki34@spaces.ru>
2015-04-21 13:21:28 +0300

    HFForum Send Request

--- src/ZectranetBundle/Controller/HeaderForumController.php
+++ src/ZectranetBundle/Controller/HeaderForumController.php
@@ -236,7 +236,7 @@ class HeaderForumController extends Controller {
         $info['HO_Contacts'] = HFForum::getNotProjectHomeOfficeMembers($em, $user->getId(), $project_id);
         $info['All_Contacts'] = HFForum::getNotProjectSiteMembers($em, $project_id);
         $info['Project_Team'] = EntityOperations::arrayToJsonArray(
-            $em->getRepository('ZectranetBundle:Request')->findBy(array('projectid' => $project_id))
+            $em->getRepository('ZectranetBundle:Request')->findBy(array('HFForumID' => $project_id))
         );
         return new JsonResponse($info);
     }
