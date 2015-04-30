2da257855ddaec03812cf74c4fcec9a42d3faa69
riki343 <riki34@spaces.ru>
2015-04-30 13:12:22 +0300

    add field date to requests

--- src/ZectranetBundle/Entity/Request.php
+++ src/ZectranetBundle/Entity/Request.php
@@ -417,7 +417,9 @@ class Request
             'id' => $this->getId(),
             'type' => $this->getType()->getInArray(),
             'status' => $this->getStatus()->getInArray(),
-            'date' => $this->getDate()->format('Y-m-d H:i:s'),
+            'date' => ($this->getDate())
+                ? $this->getDate()->format('Y-m-d H:i:s')
+                : null,
             'user' =>$this->getUser()->getInArray(),
             'project' => (($this->getType()->getLabel() == 'request_user_project')
                 or ($this->getType()->getLabel() == 'request_project'))
