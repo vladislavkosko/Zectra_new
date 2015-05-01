415d55c961f2bf55207a59eed672ec382d627698
riki343 <riki34@spaces.ru>
2015-05-01 18:18:18 +0300

    ID of task in the header of the page

--- web/bundles/zectranet/js/custom/sprintController.js
+++ web/bundles/zectranet/js/custom/sprintController.js
@@ -5,10 +5,40 @@ var sprintController = Zectranet.controller('SprintController', ['$scope', '$htt
         $scope.urlShowTask = JSON_URLS.showTask;
         $scope.urlAsset = JSON_URLS.asset;
 
+        $scope.totalEstimation = {
+            'h': 0,
+            'm': 0
+        };
+
+        function calcuateTotalEstimation(tasks) {
+            var est = {
+                'h': 0,
+                'm': 0
+            };
+
+            for (var i = 0; i < tasks.length; i++) {
+                if (!tasks[i].parentid) {
+                    est.h += tasks[i].estimatedHours;
+                    est.m += tasks[i].estimatedMinutes;
+                    for (var j = 0; j < tasks[i].subtasks.length; j++) {
+                        est.h += tasks[i].subtasks[j].estimatedHours;
+                        est.m += tasks[i].subtasks[j].estimatedMinutes;
+                    }
+                }
+            }
+
+            est.h += est.m / 60;
+            est.h = ~~est.h;
+            est.m = est.m % 60;
+            est.m = ~~est.m;
+            return est;
+        }
+
         $scope.getTasks = function () {
             $scope.promise = $http.get($scope.urlGetTasks)
                 .success(function (response) {
                     $scope.tasks = $scope.prepareTasks(response.Tasks);
+                    $scope.totalEstimation = calcuateTotalEstimation($scope.tasks);
                 }
             );
         };
